eba634f595c0f01d877869432de15492d8328bf8
t00la7y <mpiloshezi002@gmail.com>
2026-05-11 11:29:42 +0200

    chore: remove dist from tracking

--- server/dist/middleware/errorhandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
-};
-exports.default = errorHandler;
-//# sourceMappingURL=errorhandler.js.map
--- server/dist/types/userProfile.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Note: chat interactions are stored in a dedicated ChatHistory collection instead of on the user document.
-//# sourceMappingURL=userProfile.js.map
